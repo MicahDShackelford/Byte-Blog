51313da246294aff59f09749722290c01ade853d
Micah Shackelford <micahdshackelford@gmail.com>
2019-12-08 15:29:06 -0500

    Setup webpack, babel, & dependencies

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/public');
+
+module.exports = {
+  mode: 'development',
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module : {
+    rules : [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test : /\.js?/,
+        include : SRC_DIR,
+        loader : 'babel-loader',
+        query: {
+          presets: ['@babel/preset-react', '@babel/preset-env']
+        }
+      }
+    ]
+  }
+};
