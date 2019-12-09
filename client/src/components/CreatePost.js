ec540a67599404c98667a77c9bb38bd9533c32e1
Micah Shackelford <micahdshackelford@gmail.com>
2019-12-08 22:10:25 -0500

    Complete inital preview (shows post and title for now), plan to add user and date information later

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
+import PostPreview from './PostPreview';
 import './styles/CreatePost.css';
 
 
 let CreatePost = () => {
-  return (
-    <div id="create-post">
-      <form>
-        <div className="form-group">
-          <label htmlFor="post-title">Title</label>
-          <input type="text" id="post-title"></input>
-        </div>
-        <div className="form-group">
-          <label htmlFor="post-title">Body</label>
-          <textarea id="post-body"></textarea>
-        </div>
-        <div className="button-group">
-          <button>Preview Post</button>
-          <button>Submit Post</button>
-        </div>
-      </form>
-    </div>
-  )
+  const [post, setPost] = useState({});
+  const [view, setView] = useState("createpost");
+
+  let previewHandler = (e) => {
+    e.preventDefault();
+    setPost({
+      name: "undefined",
+      createdAt: "undefined",
+      title: e.target.parentNode.parentNode[0].value,
+      body: e.target.parentNode.parentNode[1].value
+    })
+    setView("postpreview");
+  }
+
+  if(view === "createpost") {
+    return (
+      <div id="create-post">
+        <form>
+          <div className="form-group">
+            <label htmlFor="post-title">Title</label>
+            <input type="text" id="post-title"></input>
+          </div>
+          <div className="form-group">
+            <label htmlFor="post-title">Body</label>
+            <textarea id="post-body"></textarea>
+          </div>
+          <div className="button-group">
+            <button onClick={previewHandler}>Preview Post</button>
+            <button>Submit Post</button>
+          </div>
+        </form>
+      </div>
+    )
+  }else if(view === "postpreview") {
+    return (
+      <PostPreview post={post} />
+    )
+  }
 }
 
 export default CreatePost;
---
+++ client/src/components/PostPreview.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles/PostPreview.css';
+
+let PostPreview = (props) => {
+  let formattedBody = props.post.body.split('\n');
+  return (
+    <div id="post-preview">
+      <h1>{props.post.title}</h1>
+      {formattedBody.map((block, i) =>(
+        <div key={`preview-post-block-${i}`}>
+          <p>{block}</p>
+          <br />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default PostPreview;
