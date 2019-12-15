850ea877d5262e4afecc42c2bd35d3f908df39c8
Micah Shackelford <micahdshackelford@gmail.com>
2019-12-14 22:22:05 -0500

    Got a post

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
-const express = require("express");
-const jwt = require("jsonwebtoken");
-const User = require("./db/user");
-const Post = require("./db/post");
-const Database = require("./db/database");
-const TokenVerify = require("./auth/tokenVerification");
-const path = require("path");
+const express     = require("express"),
+      jwt         = require("jsonwebtoken"),
+      User        = require("./db/user"),
+      Post        = require("./db/post"),
+      Comment     = require("./db/comments"),
+      Database    = require("./db/database"),
+      Counter     = require("./db/counter"),
+      TokenVerify = require("./auth/tokenVerification"),
+      path        = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,13 +16,53 @@ app.use(express.static("./client/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+app.get("/comments/retrieve/:commentId", (req, res, next) => {
+  Comment.find({postId: req.params.commentId})
+  .sort({ postedTime: 1 })
+  .then(response => {
+    res.send(response);
+  });
+});
+
+
+app.get("/users/retrieve/:username", (req, res, next) => {
+  User.findOne({username: req.params.username})
+  .then(response => {
+    res.send(response);
+  });
+});
+
 app.get("/posts/retrieve", (req, res, next) => {
   Post.find({})
-    .sort({ postedTime: 1 })
-    .then(response => {
-      res.send(response);
-    });
+  .sort({ postedTime: 1 })
+  .then(response => {
+    res.send(response);
+  });
 });
+
+app.post("/post/create", (req,res,next) => {
+  Counter.findOne({model: "post"}, (err, count) => {
+    let post = new Post({
+      id: count.count,
+      title: "test",
+      topic: "General",
+      author: "micah",
+      post: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde corporis blanditiis quis cum porro ipsam ea, magnam fugiat quasi totam dolore impedit magni, esse vitae iure deserunt vero, sapiente sequi.
+      Modi deserunt quis odio aut ut. Recusandae ab eius doloremque officiis inventore ullam voluptates ut error laboriosam ea iusto neque repellendus vero a obcaecati praesentium magni tenetur esse, explicabo quae!
+      Magnam amet ipsa nostrum, obcaecati voluptatum totam reiciendis quaerat a porro ea officiis reprehenderit aliquam eum dignissimos doloremque eligendi nemo cupiditate aperiam veniam, consequuntur laboriosam debitis odio accusantium. Voluptatibus, reprehenderit?`
+    })
+    count.count++;
+    count.save();
+    post.save();
+  });
+
+
+  res.send({
+    ok: true,
+    message: "Post Success"
+  });
+})
 
 app.get("/post/retrieve/:postId", (req, res, next) => {
   Post.find({ id: req.params.postId }).then(response => {
@@ -75,31 +117,6 @@ app.post("/auth/login", (req, res, next) => {
       }
     }
   });
-
-  // if(req.body.username === "micah" && req.body.password === "1234") {
-  //   jwt.sign({username: req.body.username}, 'secretkey', { expiresIn: '10h' }, (err, token) => {
-  //     if(err) {
-  //       res.send("Error")
-  //     } else {
-  //       res.send({
-  //         ok: true,
-  //         message: "Login success",
-  //         token,
-  //         user : {
-  //           loginStatus: true,
-  //           name: "Micah",
-  //           username: "micahdshackelford",
-  //           role:3
-  //         }
-  //       });
-  //     }
-  //   })
-  // }else {
-  //   res.send({
-  //     ok: false,
-  //     message: "Incorrect username or password"
-  //   });
-  // }
 });
 
 app.get("/create", (req, res, next) => {
